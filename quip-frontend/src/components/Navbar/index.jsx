41fb544e7c3893697464da3a9dfeeb1d0b043349
AryanAg08 <aryangoyal90@outlook.com>
2024-06-24 22:21:09 +0530

    Creation of Sidebar and Navbar folders

---
+++ quip-frontend/src/components/Navbar/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export function Navbar () {
+  {
+    return (
+      <div>This is navbar</div>
+    )
+  }
+};
---
+++ quip-frontend/src/components/sidebar/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export function SideBar () {
+  {
+    return (
+      <div>Sidebar</div>
+    )
+  }
+}
