5e65db6858e2b93a99fe66ade84e62e0f6c4a3d1
AryanAg08 <aryangoyal90@outlook.com>
2024-06-28 11:43:00 +0530

    study Time Card

--- quip-frontend/src/Pages/Dashboard/index.jsx
+++ quip-frontend/src/Pages/Dashboard/index.jsx
@@ -7,12 +7,13 @@ import Card from "../../components/Card";
 import BarChart from "../../components/Charts";
 import { barChartDataDashboard, barChartOptionsDashboard } from "../../data/chart";
 import { MdRocketLaunch } from "react-icons/md";
+import { IoCubeSharp } from "react-icons/io5";
 
 export function DashboardPage() {
     const StudyTimeCard = {
         top: "-60px",
-        left: "80%",
-        height: "50vh",
+        left: "60%",
+        height: "45vh",
         width: "30vw",
         bg: "#D4ECCD",
     }
@@ -38,13 +39,20 @@ export function DashboardPage() {
                     <p>
                         <span class="green-text">(+23%)</span> than last week
                     </p>
-
+                     <div className="study-container">
                     <div className="Study-sessions">
                         <span className="icon-box">
                             <MdRocketLaunch />
                         </span>
                         <span style={{ fontWeight: "bold", marginRight: "4px" }}>32</span> Study Sessions
                     </div>
+                    <div className="Study-rooms">
+                        <span className="icon-box-room">
+                            <IoCubeSharp />
+                        </span>
+                        <span style={{ fontWeight: "bold", marginRight: "4px" }}>32</span> Study Rooms joined
+                    </div>
+                </div>
                 </div>
             </Card>
         </>
