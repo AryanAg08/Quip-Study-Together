6bdef00061c3a9747e1ca27cbff63464f373d600
TanyaSingh103 <tanya.103.singh@gmail.com>
2024-07-01 23:46:48 +0530

    created leaderboard

--- quip-frontend/src/data/ranks.js
+++ quip-frontend/src/data/ranks.js
@@ -1,5 +1,11 @@
-export const LeaderboardDataDashboard = [ 
-    { rank: 1, name: "Alice", points: 1500, avatar: "/src/assets/user.png" },
-    { rank: 2, name: "Bob", points: 1400, avatar: "/src/assets/user.png" },
-    { rank: 3, name: "Charlie", points: 1300, avatar: "/src/assets/user.png" }
+export const leaderboardDataDashboard = [ 
+    { rank: 1, name: "Alice", points: 150, avatar: "/src/assets/user.png" },
+    { rank: 2, name: "Bob", points: 140, avatar: "/src/assets/user.png" },
+    { rank: 3, name: "Charlie", points: 130, avatar: "/src/assets/user.png" },
+    { rank: 4, name: "Alice", points: 120, avatar: "/src/assets/user.png" },
+    { rank: 5, name: "Bob", points: 100, avatar: "/src/assets/user.png" },
+    { rank: 6, name: "Charlie", points: 80, avatar: "/src/assets/user.png" },
+    { rank: 7, name: "Alice", points: 75, avatar: "/src/assets/user.png" },
+    { rank: 8, name: "Bob", points: 60, avatar: "/src/assets/user.png" },
+    { rank: 9, name: "Charlie", points: 50, avatar: "/src/assets/user.png" }
 ];
