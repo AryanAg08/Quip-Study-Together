772230cbea5872065843b85341e08041404506ed
AryanAg08 <aryangoyal90@outlook.com>
2024-07-02 10:55:41 +0530

    working on webSockets

--- backend/index.js
+++ backend/index.js
@@ -20,9 +20,27 @@ const io = new Server(server, {
     methods: ["GET", "POST"]
   },
 });
-
+var userConnections = []; // afterwards change it to database format!!
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
+  socket.on("userconnect", (data) => {
+    console.log("userconnect", data);
+    var other_users = userConnections.filter(
+      (p) => p.meeting_id == data.meetingId
+    );
+    userConnections.push({
+      connectionId: socket.id,
+      user_id: data.displayName,
+      meeting_id: data.meetingId,
+    });
+
+    other_users.forEach((v) => {
+      socket.to(v.connectionId).emit("inform_others_about_me", {
+        other_user_id: data.displayName,
+        connId: socket.id,
+      });
+    });
+  });
 
   socket.on("disconnect", () => {
     console.log("user disconnected", socket.id);
--- quip-frontend/src/Call/index.jsx
+++ quip-frontend/src/Call/index.jsx
@@ -4,18 +4,18 @@ import io from "socket.io-client";
 const socket = io("http://localhost:5001");
 
 export function Call () {
-   const sendMessage = () => {
-    // socket.emit();
-    socket.on("connect", () => {
-        console.log("Connected to server!!", socket.id)
-    });
+//    const sendMessage = () => {
+//     // socket.emit();
+//     socket.on("connect", () => {
+//         console.log("Connected to server!!", socket.id)
+//     });
 
-    socket.on("disconnect", () => {
-        console.log("disconnected from server");
-      });
-   }
+//     socket.on("disconnect", () => {
+//         console.log("disconnected from server");
+//       });
+//    }
 
-sendMessage();
+// sendMessage();
     const navigate = useNavigate();
 
     const [searchParams] = useSearchParams();
@@ -42,9 +42,30 @@ sendMessage();
 }
 
 var Myapp = (function() {
-
+    var user_id;
+    var meeting_id;
     function init(uid, mid) {
-       
+        user_id = uid;
+        meeting_id = mid;
+      event_process_for_signaling_server();
+    }
+
+    function event_process_for_signaling_server() {
+        socket.on("connect", () => {
+            if (socket.connected) {
+    
+                if (user_id != "" && meeting_id != "" ) {
+                    socket.emit("userconnect", {
+                      displayName: user_id,
+                      meetingId: meeting_id
+                    })
+                }
+            }
+           });
+
+           socket.on("inform_others_about_me", function(data) {
+             addUser(data.other_user_id, data.connId)
+           })
     }
 
    return {
